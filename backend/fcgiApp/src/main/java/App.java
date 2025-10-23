91268cc0b101257027285710ceec2550c6a8cecb
SidorovIlya <sidorov02005@icloud.com>
2025-10-24 02:23:14 +0300

    feat(web,fcgi): html-minimal — базовая страница и FCGI-скелет
    - frontend: index.html (header, X/Y/R), создан задел под index.css и index.js
    
    - backend: App.java (внесены изменения)

--- backend/fcgiApp/src/main/java/App.java
+++ backend/fcgiApp/src/main/java/App.java
@@ -1,14 +1,20 @@
 import com.fastcgi.*;
+import java.lang.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
 public class App {
     public static void main(String[] args) {
-        System.setProperty("FCGI_PORT", "29299");
-        var fcgiInterface = new FCGIInterface();
-        while (fcgiInterface.FCGIaccept() >= 0) {
-            var method = FCGIInterface.request.params.getProperty("REQUEST_METHOD");
 
-            if (method.equals("GET")) {
-                System.out.println("Content-type: text/plain\n");
-                System.out.println("Hello from java ");
+        Logger logger = Logger.getLogger(App.class.getName());
+        FileHandler fh = new FileHandler("/home/studs/s467141/httpd-root/fcgi-bin/my_log.log", false);
+        logger.addHandler(fh);
+
+        FCGIInterface fcgiInterface = new FCGIInterface();
+        logger.info("Server was started");
+        
+        while (fcgiInterface.FCGIaccept() >= 0) {
+
             }
         }
     }
